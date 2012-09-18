85c581b76ae5b9fb6872b36d586997ee83e463d3
Peter Tseng <patseng@stanford.edu>
2012-09-17 17:58:30 -0700

    Now we store the email and the location in the database :)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120917192257) do
+ActiveRecord::Schema.define(:version => 20120918004741) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(:version => 20120917192257) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "email"
+    t.string   "location"
   end
 
 end
