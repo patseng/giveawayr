06cf40c2ceab1a121b6120b440e38e61639ec357
Michael Tseng <matseng@gmail.com>
2012-09-18 22:21:52 -0700

    Created ajax post request to enter a user into a giveaway via the entry controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,17 @@
 class EntriesController < ApplicationController
   def create
-    facebook_id = params[:fb_id]
+    
+    facebook_id = params[:facebook_uid]
+    # giveaway_id = params[:giveaway_id]
+    
+    # look up the user with that facebook_id
+    user = User.where({:uid => facebook_id, :provider => "facebook"}).first
+  
+    # TODO: giveaway_id should be in the url parameters
+    
+    entry = Entry.new({user: user, giveaway: Giveaway.first, points: 1})
+    entry.save
+    
     render :nothing => true
   end
 end
